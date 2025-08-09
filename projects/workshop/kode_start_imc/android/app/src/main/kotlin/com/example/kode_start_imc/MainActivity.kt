807638a21c37adc70a7eeeac42e2ff1d90259511
Michelle Lang Baratto <mi.baratto@gmail.com>
2025-08-10 00:30:56 +0200

    apagar arquivos anteriores

--- projects/workshop/kode_start_imc/android/app/src/main/kotlin/com/example/kode_start_imc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kode_start_imc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projects/workshop/kode_start_tmdb/android/app/src/main/kotlin/com/example/kode_start_tmdb/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.kode_start_tmdb
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
